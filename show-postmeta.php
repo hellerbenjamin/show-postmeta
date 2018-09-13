57d800e9a7c9b4ea93bbce74a2bfcd5ad00c3462
Benjamin Heller <ben@benjaminheller.net>
2018-09-13 14:55:41 -0800

    plugin comment header

---
+++ show-postmeta.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Plugin Name
+*
+* @package     WP show postmeta
+* @author      Benjamin Heller
+* @copyright   2016 Your Name or Company Name
+* @license     GPL-2.0+
+*
+* @wordpress-plugin
+* Plugin Name: WP Show Postmeta
+* Description: Show postmeta on backend post edit
+* Version:     0.1.0
+* Author:      Benjamin Heller
+* Author URI:  https://benjaminheller.net
+* Text Domain: wp-show-postmeta
+* License:     GPL-2.0+
+* License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
